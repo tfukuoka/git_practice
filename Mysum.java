79073cb08e4615f243edbacb482e8b9f950db523
tfukuoka <tfukuoka@crooz.co.jp>
2015-11-30 18:36:41 +0900

    assignment submission

---
+++ Mysum.java
@@ -0,0 +1,19 @@
+﻿/**
+ * 足し算の例
+ * 
+ *@since     2015/11/30
+ *@author    fukuokatomohiro
+ */
+
+public class Mysum { 
+  public static void main(String[] args){
+     int a;
+     int b;
+     int c;
+     a = 1;
+     b = 4;
+     c = a + b;
+    System.out.println("計算結果:"+c);
+
+  }
+}
