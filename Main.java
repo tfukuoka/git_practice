b163422b31930a39d032ef30135e100e2e7b51a3
tfukuoka <tfukuoka@crooz.co.jp>
2015-12-02 01:29:48 +0900

    ryoukun hensuukeisan

---
+++ Main.java
@@ -0,0 +1,12 @@
+﻿public class Main { 
+  public static void main(String[] args){
+    System.out.println("遼君かっこいい最高！");
+    System.out.println("かっこよくもないし最高でもないよ");
+    System.out.println("遼君は引き算もやるよ");
+    System.out.println("ダブルコーテーション外すと計算できる");
+    System.out.println(10 - 3);
+    int x;
+    x = 2;
+    System.out.println(x*x);
+  }
+} 
