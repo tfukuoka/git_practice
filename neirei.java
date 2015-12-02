7155b7d7292532a30cf90029763be288d4c68f22
tfukuoka <tfukuoka@crooz.co.jp>
2015-12-03 01:15:05 +0900

    zituha22

---
+++ neirei.java
@@ -0,0 +1,9 @@
+﻿public class Main { 
+  public static void main(String[] args){
+    int age = 35;
+    System.out.println("私の年齢は" + age);
+    age = 22;
+    System.out.println("実は私の年齢は" + age);
+
+  }
+}
